08532428dbd4708b2680beefc683135d182dadd5
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-06 13:15:22 +0300

    User Class is added

---
+++ User.java
@@ -0,0 +1,12 @@
+package Project;
+
+import java.util.*;
+
+public class User 
+{
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private Date birthday;
+}
