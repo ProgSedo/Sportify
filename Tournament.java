44590df3e74ff923b9d4d2968e30830b0e09fed1
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-06 18:04:10 +0300

    Tournament updated

--- Tournament.java
+++ Tournament.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 
 public class Tournament extends Events{
     
 
     private ArrayList<User> participantList;
+    private ArrayList<User> activePlayers;
 
     public Tournament(Date eventTime, String eventName, SportType sportType) {
         super(eventTime, eventName, sportType);
         participantList = new ArrayList<>();
-        
+        activePlayers = new ArrayList<>();
     }
 
     public ArrayList<User> getParticipantList() {
@@ -22,6 +25,11 @@ public class Tournament extends Events{
 
     public String toString() {
         return super.toString() + " Tournament";
+    }
+
+    public ArrayList<User> shuffleActivePlayers(ArrayList<User> list) {
+        Collections.shuffle(list);
+        return list;
     }
     
 }
