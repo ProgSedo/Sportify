6179a74b76ec0f12646cf25b46be528d0b2df60f
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-06 17:16:44 +0300

    Events class added

---
+++ Events.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+ enum SportType {football, volleyball, tennis};
+
+ public abstract class Events {
+    
+
+    private Date eventTime;
+    private String eventName;
+    private SportType sportType;
+
+    public Events (Date eventTime, String eventName, SportType sportType) {
+        this.eventTime = eventTime;
+        this.eventName = eventName;
+        this.sportType = sportType;
+    }
+
+    public Date getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(Date eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    
+    public SportType getSportType() {
+        return sportType;
+    }
+
+    public void setSportType(SportType sportType) {
+        this.sportType = sportType;
+    }
+}
