22ff2abe2183b711876d546ab4b928830278fd17
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-06 13:48:05 +0300

    Added instance variables

--- User.java
+++ User.java
@@ -17,7 +17,20 @@ public class User
     private boolean tennis;
 
     private ArrayList<User> friends;
+    private ArrayList<User> friendRequests;
     private ArrayList<Event> eventsEngagedIn;
+
+    public User(String userEmail, String userPassword, Date dateOfBirth
+                boolean isFootball, boolean isVolleyball, boolean isTennis)
+    {
+        email = userEmail;
+        password = userPassword;
+        birthday = dateOfBirth;
+        football = isFootball;
+        volleyball = isVolleyball;
+        tennis = isTennis;
+    }
+
 
 
 }
