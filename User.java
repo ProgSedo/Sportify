4da0805154a8b822835ade2e1885c5c993b9d1c2
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-06 13:29:48 +0300

    Added instance variables

--- User.java
+++ User.java
@@ -9,7 +9,15 @@ public class User
     private String name;
     private String surname;
     private Date birthday;
+    private String aboutMe;
+    private Image profilePhoto;
+
     private boolean football;
     private boolean volleyball;
     private boolean tennis;
+
+    private ArrayList<User> friends;
+    private ArrayList<Event> eventsEngagedIn;
+
+
 }
