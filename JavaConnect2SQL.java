86b9abe7387f33b25532e941bd23e7faa6a8a9b6
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-08 00:15:39 +0300

    sql attempt

---
+++ JavaConnect2SQL.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JavaConnect2SQL {
+    public static void main(String[] args) {
+       
+        
+                String connectionString = "jdbc:sqlserver://MSI;database=UsersDB;IntegratedSecurity=true;trustServerCertificate=true;";
+                //String user = "sa";
+                //String password = "1";
+        
+                try {
+                    System.out.println("Attempting to establish connection...");
+                    try (Connection connection = DriverManager.getConnection(connectionString)) {
+                        System.out.println("Connection established successfully!");
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Error: Failed to establish connection.");
+                    e.printStackTrace();
+                }
+            
+        
+        
+        
+        
+    }
+}
