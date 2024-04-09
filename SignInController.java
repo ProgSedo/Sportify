8d245d3d2b6dc71e04a11d3faa65a564d5af8126
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-09 18:31:52 +0300

    Added Sign In Page UI

---
+++ SignInController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SignInController {
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    void registerButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInButtonClicked(ActionEvent event) {
+
+    }
+
+}
