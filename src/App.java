231046636f92bede4562fa29c2bcb83a819759c0
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-10 21:39:19 +0300

    Added both SDK 17 and 22

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    
+}
