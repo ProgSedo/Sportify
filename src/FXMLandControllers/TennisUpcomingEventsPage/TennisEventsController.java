a0e18c1cb5935dce2ac26246d372544bb88cee61
Nevzat <eren.tezcan@ug.bilkent.edu.tr>
2024-04-12 22:53:38 +0300

    FXML files and controllers added for the 3 upcomig events pages

---
+++ src/FXMLandControllers/TennisUpcomingEventsPage/TennisEventsController.java
@@ -0,0 +1,16 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TennisEventsController {
+
+    @FXML
+    void TennisEventsBackButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TennisEventsSideBar(ActionEvent event) {
+
+    }
+
+}
