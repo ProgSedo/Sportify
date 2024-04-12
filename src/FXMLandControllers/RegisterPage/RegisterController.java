e64eb4e492d41708286b227e1bf80e03d0de219b
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-12 14:35:27 +0300

    Added register page and controller

---
+++ src/FXMLandControllers/RegisterPage/RegisterController.java
@@ -0,0 +1,60 @@
+package FXMLandControllers.RegisterPage;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class RegisterController {
+
+    @FXML
+    private TextField confirmPasswordTextField;
+
+    @FXML
+    private Text dateOfBirthText;
+
+    @FXML
+    private ComboBox<?> dayComboBox;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private CheckBox footballCheckBox;
+
+    @FXML
+    private Text interestsText;
+
+    @FXML
+    private ComboBox<?> monthComboBox;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Text resgisterText;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Text sportifyText;
+
+    @FXML
+    private CheckBox tennisCheckBox;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private CheckBox volleyballCheckBox;
+
+    @FXML
+    private ComboBox<?> yearComboBox;
+
+}
