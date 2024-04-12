5be38a9144bf80634da6bacb3b6454083079c814
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-12 21:02:25 +0300

    Sign in completed

--- src/FXMLandControllers/SignInPage/SignInController.java
+++ src/FXMLandControllers/SignInPage/SignInController.java
@@ -1,15 +1,24 @@
 package FXMLandControllers.SignInPage;
 
+import java.io.IOException;
+
 import Models.Database;
 import Models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class SignInController {
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private TextField emailTextField;
@@ -38,11 +47,15 @@ public class SignInController {
     }
 
     @FXML
-    void signInButtonClicked(ActionEvent event) {
+    void signInButtonClicked(ActionEvent event) throws IOException{
         if (Database.checkEmail(emailTextField.getText())) {
             if (Database.checkPassword(emailTextField.getText(), passwordTextField.getText())) {
                 User user = Database.getUser(emailTextField.getText());
-                //TBC by ata
+                Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+                 stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                 scene = new Scene(root);
+                 stage.setScene(scene);
+                 stage.show();
             }
         }
     }
