d17dd83b9ccd84b589b5cecc4fd94074ff66596a
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-12 14:08:38 +0300

    Added sign in page fxml and controller
    It works

--- src/App.java
+++ src/App.java
@@ -1,19 +1,26 @@
+import FXMLandControllers.SignInPage.SignInController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLandControllers/SignInPage/SignIn.fxml"));
+        SignInController controller = new SignInController();
+        loader.setController(controller);
+        Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         launch();
     }
     
---
+++ src/FXMLandControllers/SignInPage/SignInController.java
@@ -0,0 +1,42 @@
+package FXMLandControllers.SignInPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SignInController {
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Text getStartedText;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Text sportifyText;
+
+    @FXML
+    private Text welcomeText;
+
+    @FXML
+    void registerButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInButtonClicked(ActionEvent event) {
+
+    }
+
+}
