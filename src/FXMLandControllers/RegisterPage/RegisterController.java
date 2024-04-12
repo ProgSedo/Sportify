3bd99b23eb15034234174ffd02c98877dc1ecb02
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-12 21:59:28 +0300

    Register in progress

--- src/FXMLandControllers/RegisterPage/RegisterController.java
+++ src/FXMLandControllers/RegisterPage/RegisterController.java
@@ -1,14 +1,26 @@
 package FXMLandControllers.RegisterPage;
 
+import java.io.IOException;
+
+import Models.Database;
+import Models.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class RegisterController {
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private TextField confirmPasswordTextField;
@@ -17,7 +29,7 @@ public class RegisterController {
     private Text dateOfBirthText;
 
     @FXML
-    private ComboBox<?> dayComboBox;
+    private ComboBox<Integer> dayComboBox;
 
     @FXML
     private TextField emailTextField;
@@ -29,7 +41,7 @@ public class RegisterController {
     private Text interestsText;
 
     @FXML
-    private ComboBox<?> monthComboBox;
+    private ComboBox<Integer> monthComboBox;
 
     @FXML
     private TextField passwordTextField;
@@ -56,16 +68,49 @@ public class RegisterController {
     private CheckBox volleyballCheckBox;
 
     @FXML
-    private ComboBox<?> yearComboBox;
+    private ComboBox<Integer> yearComboBox;
 
     @FXML
-    void registerButtonClicked(ActionEvent event) {
-
+    void registerButtonClicked(ActionEvent event) throws IOException{
+        String email = emailTextField.getText();
+        String password = passwordTextField.getText();
+        if (!Database.checkEmail(email)) {
+            if (password.equals(confirmPasswordTextField.getText())) {
+                User user = new User(email, password, yearComboBox.getValue(), isFootballSelected(), isVolleyballelected(), isTennisSelected());
+                //LATER GOING TO BE USED ON APP
+                Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        }
     }
 
     @FXML
-    void signInClicked(ActionEvent event) {
+    void signInClicked(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    private boolean isTennisSelected() {
+        if (tennisCheckBox.isSelected())
+            return true;
+            return false;
+    }
+
+    private boolean isFootballSelected() {
+        if (footballCheckBox.isSelected())
+            return true;
+            return false;
+    }
+
+    private boolean isVolleyballelected() {
+        if (volleyballCheckBox.isSelected())
+            return true;
+            return false;
     }
 
 }
