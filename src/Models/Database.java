8b4257460b22a57f532938e7f405671136f7d2d3
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-04-12 16:04:04 +0300

    database conenction method

---
+++ src/Models/Database.java
@@ -0,0 +1,12 @@
+package Models;
+
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Database {
+    
+}
--- src/Models/JavaConnect2SQL.java
+++ src/Models/JavaConnect2SQL.java
@@ -5,28 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JavaConnect2SQL {
-    public static void main(String[] args) {
-       
-        
-                String connectionString = "jdbc:sqlserver://MSI;DatabaseName=UsersDB;IntegratedSecurity=true;TrustServerCertificate=True";
-                //String connectionString = "jdbc:sqlserver://MSI;DatabaseName=UsersDB;IntegratedSecurity=true;TrustServerCertificate=True;user=sa\\username;password=1";
 
-                String user = "sa";
-                String password = "1";
-        
-                try {
-                    System.out.println("Attempting to establish connection...");
-                    try (Connection connection = DriverManager.getConnection(connectionString, user, password)) {
-                        System.out.println("Connection established successfully!");
-                    }
-                } catch (SQLException e) {
-                    System.out.println("Error: Failed to establish connection.");
-                    e.printStackTrace();
-                }
-            
-        
-        
-        
-        
+    public static String connectionString = "jdbc:sqlserver://MSI;DatabaseName=UsersDB;IntegratedSecurity=true;TrustServerCertificate=True";
+    // String connectionString =
+    // "jdbc:sqlserver://MSI;DatabaseName=UsersDB;IntegratedSecurity=true;TrustServerCertificate=True;user=sa\\username;password=1";
+
+    static String user = "sa";
+    static String password = "1";
+
+
+    public static Connection connect2Database() {
+        try {
+            Connection connection = DriverManager.getConnection(connectionString, user, password);
+            //System.out.println("Attempting to establish connection...");
+                return connection;
+        } catch (SQLException e) {
+            //System.out.println("Error: Failed to establish connection.");
+            e.printStackTrace();
+        }
+        return null;
     }
 }
