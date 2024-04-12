b4d4d7d911e3dff60f27d839ffb6395e055eae6b
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-12 18:18:15 +0300

    Some changes

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import FXMLandControllers.AddFriendPage.AddFriendController;
 import FXMLandControllers.HomePage.HomePageController;
 import FXMLandControllers.SignInPage.SignInController;
 import javafx.application.Application;
@@ -19,6 +20,7 @@ public class App extends Application{
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args) throws Exception{
