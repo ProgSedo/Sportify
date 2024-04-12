3032dccffe3b6f9d6bd8f5f51e3a0d7e043683f9
Berke Asil <berkeasil2004@gmail.com>
2024-04-12 22:51:29 +0300

    Designs of football tennis and voleyball forums are added

---
+++ src/FXMLandControllers/FootballForumPage/FootballForumController.java
@@ -0,0 +1,28 @@
+package FXMLandControllers.FootballForumPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class FootballForumController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextArea footBallForumTextArea;
+
+    @FXML
+    private Label footballTitleLabel;
+
+    @FXML
+    private Button menuBarButton;
+
+    @FXML
+    void menuBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
---
+++ src/FXMLandControllers/VolleyballForumPage/VoleyballForumController.java
@@ -0,0 +1,28 @@
+package FXMLandControllers.VolleyballForumPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class VoleyballForumController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button menuBarButton;
+
+    @FXML
+    private TextArea voleyballForumTextArea;
+
+    @FXML
+    private Label voleyballTitleLabel;
+
+    @FXML
+    void menuBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
