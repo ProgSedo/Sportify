ecb76e6c183469ea2441f1c99bfe4fdbca917631
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-15 00:24:03 +0300

    Added Profile and Settings Page

---
+++ src/FXMLandControllers/ProfilePage/ProfilePageController.java
@@ -0,0 +1,42 @@
+package FXMLandControllers.ProfilePage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ProfilePageController {
+
+    @FXML
+    private TextArea aboutMeTextArea;
+
+    @FXML
+    private TextField ageTextField;
+
+    @FXML
+    private TextArea commentsTextArea;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField interestsTextField;
+
+    @FXML
+    private ImageView profilePhoto;
+
+    @FXML
+    private Button sideBarButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void sideBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/FXMLandControllers/SettingsPage/SettingsPageController.java
@@ -0,0 +1,78 @@
+package FXMLandControllers.SettingsPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SettingsPageController {
+
+    @FXML
+    private Button aboutMeSaveButton;
+
+    @FXML
+    private TextArea aboutMeTextArea;
+
+    @FXML
+    private TextField ageTextField;
+
+    @FXML
+    private Button changePP;
+
+    @FXML
+    private Button changePasswordButton;
+
+    @FXML
+    private TextField confirmNewPasswordTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Button informationSaveButton;
+
+    @FXML
+    private TextField interestsTextField;
+
+    @FXML
+    private TextField newPasswordTextField;
+
+    @FXML
+    private ImageView profilePhoto;
+
+    @FXML
+    private Button sideBarButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void aboutMeSaveButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changePPClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changePasswordButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sideBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
+
+
