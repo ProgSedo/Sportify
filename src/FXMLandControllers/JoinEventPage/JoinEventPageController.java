79e9fae4e1437190d31ff0ba8177e0dff9f2aea9
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-04-15 16:00:18 +0300

    Added Join Event Page

---
+++ src/FXMLandControllers/JoinEventPage/JoinEventPageController.java
@@ -0,0 +1,43 @@
+package FXMLandControllers.JoinEventPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class JoinEventPageController {
+
+    @FXML
+    private TextField dateTextField;
+
+    @FXML
+    private TextArea detailsTextArea;
+
+    @FXML
+    private Button joinButton;
+
+    @FXML
+    private TextArea participantsTextArea;
+
+    @FXML
+    private TextField placeTextField;
+
+    @FXML
+    private Button sideBarButton;
+
+    @FXML
+    private TextField sportTypeTextField;
+
+    @FXML
+    void joinButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sideBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
+
