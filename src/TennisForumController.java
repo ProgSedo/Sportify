8d9129fff680e5fa0032da9bdc6db924a778c167
Berke Asil <berkeasil2004@gmail.com>
2024-04-17 14:23:59 +0300

    TennisForumController fx file is added

---
+++ src/TennisForumController.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class TennisForumController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button menuBarButton;
+
+    @FXML
+    private TextArea tennisForumTextArea;
+
+    @FXML
+    private Label tennisTitleLabel;
+
+    @FXML
+    void menuBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
