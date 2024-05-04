f40cf0811ca745a50339c313f163e5e6b176c68d
Serdar Bozdağ <serdarbozdag03@gmail.com>
2024-05-04 19:02:15 +0300

    Added getter an setter for currUserEmail

--- src/Model.java
+++ src/Model.java
@@ -24,4 +24,14 @@ public class Model {
     public ViewFactory getViewFactory() {
         return viewFactory;
     }
+
+    public String getEmail()
+    {
+        return currUserEmail;
+    }
+
+    public void setEmail(String email)
+    {
+        currUserEmail = email;
+    }
 }
