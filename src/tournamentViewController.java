24553b4e18b5406ba5a28bd0f6387d8981866368
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2024-05-12 15:12:42 +0300

    new controller

---
+++ src/tournamentViewController.java
@@ -0,0 +1,60 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class tournamentViewController {
+
+    @FXML
+    private Button announceButton;
+
+    @FXML
+    private Button joinButton;
+
+    @FXML
+    private TextArea participantsTextArea;
+
+    @FXML
+    private Button sideBarButton;
+
+    @FXML
+    private TextField start1;
+
+    @FXML
+    private TextField start2;
+
+    @FXML
+    private TextField start3;
+
+    @FXML
+    private TextField start4;
+
+    @FXML
+    private TextField start5;
+
+    @FXML
+    private TextField start6;
+
+    @FXML
+    private TextField start7;
+
+    @FXML
+    private TextField start8;
+
+    @FXML
+    void announceButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void joinButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sideBarButtonClicked(ActionEvent event) {
+
+    }
+
+}
